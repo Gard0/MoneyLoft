94a4d3c99f54dda618c1675a37a6653e3b4244cc
Майк <michael.fomin.89@gmail.com>
2019-07-04 17:55:29 +0300

    Authorization improved. Gradle build improvements. Application stability improved. (part 2)

---
+++ app/src/main/java/com/loftschool/fomin/moneyloft/AuthResponse.java
@@ -0,0 +1,31 @@
+package com.loftschool.fomin.moneyloft;
+
+class AuthResponse {
+    private String status;
+    private String id;
+    private String authToken;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+}
